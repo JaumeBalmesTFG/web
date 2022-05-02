d1b2d9a9fd9a99ddfac8904ecd595ea679375e5a
Levon <lgukasya20@jaumebalmes.net>
2022-05-02 19:28:35 +0200

    {F-19} ADD user register

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,33 @@
 import { environment } from "src/environments/environment.prod";
 
-// Auth, check if the email is ont the database
-export async function auth(email: String): Promise<String> {
-
+// Auth, check if the email is ont the database 
+export async function auth(email: string): Promise<String> {
   const options = {
     method: 'POST',
     body: JSON.stringify({"email": email}),
     headers: { 'Content-Type': 'application/json' }
   };
 
-  const data = await fetch(environment.api + "/auth", options)
-    .then((response) => { return response.json() })
+  const response = await fetch(environment.api + "/auth", options)
+    .then((res) => { return res.json() })
     .then((json) => { return json; })
     .catch(err => { return "error"; });
 
-  return data;
+  return response;
+}
+
+// Register
+export async function register(data: string): Promise<String> {
+  const options = {
+    method: 'POST',
+    body: data,
+    headers: { 'Content-Type': 'application/json' }
+  };
+
+  const response = await fetch(environment.api + "/register", options)
+    .then((res) => { return res.json() })
+    .then((json) => { return json; })
+    .catch(err => { return "error"; });
+
+  return response;
 }
