0cafd208222aa4d7b372adef36bc3e736b6b4129
Levon <lgukasya20@jaumebalmes.net>
2022-05-04 19:29:25 +0200

    [F-41] ADD delete rule

--- src/app/services/rule.service.ts
+++ src/app/services/rule.service.ts
@@ -2,6 +2,12 @@
 import { environment } from 'src/environments/environment.prod';
 import { updateFetchHeader } from './reqOptions.service';
 
+/** RULE SCHEMA
+ * ufId
+ * title
+ * percentage
+ */
+
 // Create Rule
 export async function createRule(data: any): Promise<String> {
     const header = updateFetchHeader();
@@ -47,6 +53,20 @@ export async function updateRule(data: any): Promise<String> {
     data = JSON.stringify(data);
 
     const response = await fetch(environment.api + `/rule/${ruleId}/edit`, { method: "PUT", headers: { "Content-Type": "application/json", ...header }, body: data })
+        .then(res => { return res.json() })
+        .then(json => { return json })
+        .catch(err => { return err })
+
+    return response;
+}
+
+// Delete Rule
+export async function deleteRule(data: any): Promise<String> {
+    const header = updateFetchHeader();
+
+    const response = await fetch(environment.api + `/rule/${data}/delete`, {
+        method: "DELETE", headers: { ...header }
+    })
         .then(res => { return res.json() })
         .then(json => { return json })
         .catch(err => { return err })
