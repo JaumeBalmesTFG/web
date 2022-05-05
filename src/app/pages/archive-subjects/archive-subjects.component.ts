1232d41890fe647921a85bef3c52d908f364f1d9
xavigrausebastian <xavier.grau@kreatings.com>
2022-05-05 17:39:44 +0200

    F-18 Create archived subject component

---
+++ src/app/pages/archive-subjects/archive-subjects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-archive-subjects',
+  templateUrl: './archive-subjects.component.html',
+  styleUrls: ['./archive-subjects.component.scss']
+})
+export class ArchiveSubjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/web-routing.module.ts
+++ src/app/web-routing.module.ts
@@ -5,12 +5,14 @@ import { CalendarComponent } from './pages/calendar/calendar.component';
 import { LoginComponent } from './auth/login.component';
 import { ShowGradesComponent } from './pages/show-grades/show-grades.component';
 import { SubjectsAndUfsComponent } from './pages/subjects-and-ufs/subjects-and-ufs.component';
+import {ArchiveSubjectsComponent} from "./pages/archive-subjects/archive-subjects.component";
 
 const routes: Routes = [
     {path:'login', component: LoginComponent},
     {path:'calendar', component: CalendarComponent},
     {path:'subjectsAndUfs', component: SubjectsAndUfsComponent},
     {path:'showGrades', component: ShowGradesComponent},
+    { path: 'archived', component: ArchiveSubjectsComponent},
     {path:'',redirectTo: '/calendar', pathMatch: 'full'}
 ]
 
