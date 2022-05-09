2d2a17790d4107625ddafd2d1f5dcb0369dc8658
Levon <lgukasya20@jaumebalmes.net>
2022-05-09 17:41:44 +0200

    F-53 ADD button redirect to subjects

--- src/app/pages/archive-subjects/archive-subjects.component.ts
+++ src/app/pages/archive-subjects/archive-subjects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { archiveOrDearchiveSubject, getAllArchivedSubjects } from 'src/app/services/subject.service';
 
 @Component({
@@ -8,27 +9,27 @@ import { archiveOrDearchiveSubject, getAllArchivedSubjects } from 'src/app/servi
 })
 export class ArchiveSubjectsComponent implements OnInit {
 
-    constructor() { }
+    constructor(private router: Router) { }
 
-    subjects:any = [];
+    subjects: any = [];
 
     ngOnInit(): any {
         this.callAllArchivated();
     }
 
     async callAllArchivated(): Promise<any> {
-        let archived:any = await getAllArchivedSubjects();
+        let archived: any = await getAllArchivedSubjects();
 
         this.subjects = [];
 
         archived.body.forEach((element: { name: any; _id: any; archived: boolean }) => {
-            this.subjects.push({ name: element.name, moduleId: element._id, archived: element.archived  });
+            this.subjects.push({ name: element.name, moduleId: element._id, archived: element.archived });
         });
 
         return archived;
     }
 
-    async deleteSubject(subjectToDelete:any){
+    async deleteSubject(subjectToDelete: any) {
         let res = await archiveOrDearchiveSubject(subjectToDelete);
         this.callAllArchivated();
     }
