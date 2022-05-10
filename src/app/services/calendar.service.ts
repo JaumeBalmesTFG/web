5974411403c7a966604184d6de45af2f166f3622
Levon <lgukasya20@jaumebalmes.net>
2022-05-10 17:15:24 +0200

    [FA-03] ADD get calendar service

---
+++ src/app/services/calendar.service.ts
@@ -0,0 +1,18 @@
+// Calendar Service
+import { environment } from 'src/environments/environment.prod';
+import { updateFetchHeader } from './reqOptions.service';
+
+// Get calendar
+export async function getCalendar(): Promise<String> {
+    const header = updateFetchHeader();
+    const response = await fetch(environment.api + "/calendar", { method: "GET", headers: { ...header } })
+        .then(res => {
+            return res.json() })
+        .then(json => {
+            return json })
+        .catch(err => {
+            console.log(err);
+            return err })
+
+    return response;
+}
