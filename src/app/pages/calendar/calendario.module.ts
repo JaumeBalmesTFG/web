05869d978ea9a4a4ac598d403bbe7e0fa29f74a0
alum-01 <anicol20@jaumebalmes.net>
2022-05-10 19:20:10 +0200

    Fixed calendario module conflicts

--- src/app/pages/calendar/calendario.module.ts
+++ src/app/pages/calendar/calendario.module.ts
@@ -1,12 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 @NgModule({
     declarations: [],
     imports: [
