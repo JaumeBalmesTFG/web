60e74404bbe0cd35774ddbe2e0b875b641c346ce
alum-01 <anicol20@jaumebalmes.net>
2022-05-10 19:15:35 +0200

    Fixed conflicting files

--- src/app/pages/calendar/calendario.module.ts
+++ src/app/pages/calendar/calendario.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 @NgModule({
