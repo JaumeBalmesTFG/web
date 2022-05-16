278a518e14776f1c2642ee48289331c6d32d64e7
Levon <lgukasya20@jaumebalmes.net>
2022-05-16 19:08:48 +0200

    [F-69] ADD warning messages on create task and truancy

--- src/app/modals/modal-task-truancy/modal-task-truancy.component.ts
+++ src/app/modals/modal-task-truancy/modal-task-truancy.component.ts
@@ -19,6 +19,8 @@ export class ModalTaskTruancyComponent implements OnInit {
         public dialogRef: MatDialogRef<ModalTaskTruancyComponent>
     ) { }
 
+    emptySubjects = false;
+    emptyUfs = false;
     close = false;
     formTask!: FormGroup;
     formTruancy!: FormGroup;
@@ -69,11 +71,19 @@ export class ModalTaskTruancyComponent implements OnInit {
     async callSubjects(): Promise<void> {
         let res: any = await getAllSubjects();
         this.subjects = res.body;
+
+        if(res.body.length === 0){
+            this.emptySubjects = true;
+        }
     }
 
     async callUfs(data: string): Promise<void> {
         let res: any = await getUfsFromOneModule(data);
         this.ufs = res.body;
+
+        if(this.ufs.length === 0){
+            this.emptyUfs = true;
+        }
     }
 
     async callRules(): Promise<void> {
@@ -92,7 +102,9 @@ export class ModalTaskTruancyComponent implements OnInit {
     }
 
     async createNewTask() {
+        
         this.task_data.dueDate = this.data;
+
         if (this.formTask.valid) {
             await createTask(JSON.stringify(this.task_data));
             this.dialogRef.close(true);
