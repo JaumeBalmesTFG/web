b106263f8add29cd3b72a94cbeaa300993f644b3
xavigrausebastian <xavier.grau@kreatings.com>
2022-05-19 11:42:53 +0200

    Change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/proyecto-daw'),
+      dir: require('path').join(__dirname, './coverage/k-lendar'),
       subdir: '.',
       reporters: [
         { type: 'html' },
